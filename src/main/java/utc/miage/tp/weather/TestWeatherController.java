35bf226ba7147b0412788e4a5d7cc2d3cc88fefc
Leushuis <leushuis.robbe@gmail.com>
2026-04-09 16:17:32 +0200

    fix: fix bug help not closing

--- src/main/java/utc/miage/tp/weather/TestWeatherController.java
+++ src/main/java/utc/miage/tp/weather/TestWeatherController.java
@@ -7,18 +7,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestWeatherController {
 
-    @GetMapping("/test-weather")
-    public String testWeather(Model model) {
-        WeatherStatsDTO weatherData = new WeatherStatsDTO(
-            "20",
-            "22",
-            "18",
-            "12",
-            "5",
-            "10",
-            "cloudy"
-        );
-        model.addAttribute("weatherData", weatherData);
-        return "test-page";
-    }
+  @GetMapping("/test-weather")
+  public String testWeather(Model model) {
+    WeatherStatsDTO weatherData = new WeatherStatsDTO("20", "22", "18", "12", "5", "10", "cloudy");
+    model.addAttribute("weatherData", weatherData);
+    return "test-page";
+  }
 }
