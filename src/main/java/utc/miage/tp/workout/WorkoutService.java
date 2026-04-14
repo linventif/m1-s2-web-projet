74a8492de5c6e56790c6c1fd1d91d64fefc640fb
Pham-hang269 <pmh4926@gmail.com>
2026-04-14 15:47:43 +0200

    update

--- src/main/java/utc/miage/tp/workout/WorkoutService.java
+++ src/main/java/utc/miage/tp/workout/WorkoutService.java
@@ -5,8 +5,10 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import utc.miage.tp.user.User;
 import utc.miage.tp.weather.WeatherService;
 import utc.miage.tp.weather.WeatherStatsDTO;
@@ -377,6 +379,7 @@ public class WorkoutService {
 
   private LocalDate minDate(LocalDate a, LocalDate b) {
     return a.isBefore(b) ? a : b;
+  }
   public Optional<Workout> findById(Long id) {
     return workoutRepository.findById(id);
   }
