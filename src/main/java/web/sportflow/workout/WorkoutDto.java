6bc47aafa65f74e57cc8b2844f71afdaf99082ac
Zolkn-sama <enzo.land01630@gmail.com>
2026-04-15 17:26:49 +0200

    test: space

--- src/main/java/web/sportflow/workout/WorkoutDto.java
+++ src/main/java/web/sportflow/workout/WorkoutDto.java
@@ -1,6 +1,7 @@
 package web.sportflow.workout;
 
 import java.time.LocalDateTime;
+
 import org.springframework.format.annotation.DateTimeFormat;
 import web.sportflow.sport.Sport;
 import web.sportflow.weather.WeatherStatsDTO; // Import important
@@ -24,20 +25,20 @@ public class WorkoutDto {
 
   public WorkoutDto() {
     this.weather =
-        new WeatherStatsDTO(); // Initialisation pour éviter les NullPointerException dans le form
+      new WeatherStatsDTO(); // Initialisation pour éviter les NullPointerException dans le form
   }
 
   public WorkoutDto(
-      Long id,
-      String name,
-      String description,
-      Sport sport,
-      LocalDateTime date,
-      Double rating,
-      Double duration,
-      Double distance,
-      String address,
-      WeatherStatsDTO weather) {
+    Long id,
+    String name,
+    String description,
+    Sport sport,
+    LocalDateTime date,
+    Double rating,
+    Double duration,
+    Double distance,
+    String address,
+    WeatherStatsDTO weather) {
     this.id = id;
     this.name = name;
     this.description = description;
@@ -69,6 +70,7 @@ public class WorkoutDto {
   public String getDescription() {
     return description;
   }
+
 
   public void setDescription(String description) {
     this.description = description;
